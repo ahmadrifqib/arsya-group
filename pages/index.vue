10a5ff0493c9a6b2a2aa33078de834496af4370c
Ahmad Rifqi Baidhowi <ahmad.rifqi@outlook.co.id>
2022-01-04 17:05:57 +0700

    add assets, pages, and static

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="antialiased font-sans">
+		<div class="relative bg-food bg-cover bg-center bg-no-repeat h-screen mx-auto">
+			<div class="abolute bg-gray-500 bg-opacity-70 h-screen mx-auto justify-between items-center flex flex-col">
+				<div class="bg-white flex w-full h-20 justify-between items-center">
+					<div class="bg-white md:bg-black h-full text-black md:text-white items-center flex justify-between">
+						<div class="md:bg-white pl-4 md:pl-10 pr-2">
+							<img class="h-20" src="~/assets/logo.png" alt="Logo Arsya" />
+						</div>
+						<div class="p-5 ml-2 md:mr-10 font-medium text-3xl">Arsya Group</div>
+					</div>
+					<a
+						href=""
+						class="hidden md:block font-medium text-2xl mx-20 px-3 py-1 hover:border-b-4 border-red-500 hover:text-green-600"
+						>Home</a
+					>
+				</div>
+				<div class="text-white font-mendium text-8xl md:text-9xl rounded tracking-wide">Coming Soon!</div>
+				<div></div>
+			</div>
+			<div
+				class="flex flex-col md:flex-row mx-auto justify-between items-center bg-green-600 text-white px-0 py-10 md:px-24 font-normal h-auto md:h-28"
+			>
+				<div class="mb-5 md:mb-0">&copy; 2021 Arsya Group | All rights reserved</div>
+				<div class="flex flex-row">
+					<div class="mr-3">
+						<img class="h-20" src="~/assets/logo-catering.png" alt="Logo Catering" />
+					</div>
+					<div class="mr-3">
+						<img class="h-20" src="~/assets/logo-buah.png" alt="Logo Buah" />
+					</div>
+					<div class="mr-3">
+						<img class="h-20" src="~/assets/logo-wisata.png" alt="Logo Wisata" />
+					</div>
+					<div class="mr-0">
+						<img class="h-20" src="~/assets/logo-kafe.png" alt="Logo Kafe" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
